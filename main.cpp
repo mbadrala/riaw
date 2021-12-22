2684eadef0c9df872767d39375ea2510e8eb9f7f
mbadrala <a.badrala@gmail.com>
2021-12-22 23:41:14 +0800

    Increase image width to 2560

--- main.cpp
+++ main.cpp
@@ -38,7 +38,7 @@ int main()
     // image
     
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 2560;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
